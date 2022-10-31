d8d60643787e03deaff52492a04f2a24329e8a53
Nathan Ribeiro <nathan.ribeiros7@gmail.com>
2022-10-31 02:51:16 -0300

    envio tarefa

--- pages/home/index.js
+++ pages/home/index.js
@@ -1,1 +1,95 @@
-/* Desenvolva seu script aqui */
+
+
+const baseUrl = "https://m2-api-living.herokuapp.com/news"
+
+async function getNews(){
+    
+    try{
+        const request = await fetch("https://m2-api-living.herokuapp.com/news?page=5",{
+            method: "GET",
+            headers: {
+                "Content-Type":"application/json"
+            }
+        })
+
+        const response = request.json()
+        return response
+
+    }
+    catch (err){
+        console.log(err)
+    }
+}
+
+
+
+
+
+
+
+async function renderPosts(id){
+    console.log(id)
+    let ul = document.querySelector(".posts-list")
+    ul.innerHTML = ""
+    let newsToRender = await getNews ()
+    if (id  !== "Todos"){
+         newsToRender.news = newsToRender.news.filter(element => element.category === id)
+    } 
+    
+    newsToRender.news.forEach(element  => {
+        
+        const li = document.createElement("li")
+        const img = document.createElement("img")
+        const h3 = document.createElement("h3")
+        const p = document.createElement("p")
+        const button = document.createElement("button")
+
+        img.src = element.image
+        button.id = element.id
+        li.id = element.category
+        
+        button.innerText = "Acessar conteúdo"
+        h3.innerText = element.title
+        p.innerText = element.description
+
+        li.classList.add("posts-list-item")
+        button.classList.add("access-content")
+
+
+        li.append(img,h3,p,button)
+        ul.appendChild(li)
+    })
+    
+    accessPost(newsToRender.news)
+}
+
+renderPosts("Todos")
+
+
+ function accessPost(news){
+    const buttonPost = document.querySelectorAll(".access-content")
+
+    buttonPost.forEach(element => {
+
+        element.addEventListener("click",  async () => {   
+
+            let newToOpen =  news.find(article=>article.id==element.id)
+            localStorage.setItem('@postClickedLiving', JSON.stringify(newToOpen))  
+            setTimeout (()=>{window.location.replace("/pages/post/index.html")}, 500)
+            await renderContent ()
+            
+        })
+    })
+}
+
+function filterCategorys(){
+    const categorysButtons = document.querySelectorAll(".category-list-item")
+    categorysButtons.forEach(element => {
+    element.addEventListener("click", ()=>{
+       renderPosts(element.id)
+    })
+})
+}
+filterCategorys ()
+
+export {renderPosts}
--- pages/post/index.js
+++ pages/post/index.js
@@ -1,1 +1,46 @@
-/* Desenvolva seu script aqui */
+
+
+renderContent()
+backHome ()
+
+function renderContent(){
+console.log("chegou")
+    
+const notice = JSON.parse(localStorage.getItem("@postClickedLiving"))
+  console.log(notice)
+const main = document.querySelector("main")
+const div = document.createElement("div")
+const h1 = document.createElement("h1")
+const pDescription = document.createElement("p")
+const img = document.createElement("img")
+const pContent = document.createElement("p")
+
+img.src = notice.image
+
+pContent.innerText = notice.content
+pDescription.innerText = notice.description
+h1.innerText = notice.title
+
+div.classList.add("page-title")
+
+
+
+div.append(h1,pDescription)
+main.append(div,img,pContent)
+backHome ()
+
+}
+
+
+ 
+
+function backHome (){
+    const button = document.querySelector(".back-home")
+    button.addEventListener("click", ()=>{
+        window.location.replace("/pages/home/index.html")
+    })
+}
+
+
+
+export { renderContent, backHome}
